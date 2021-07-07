1e57f49a356c69bc2f19a2b0955a167ed5448f9f
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-07-07 18:58:20 +0300

    + eslint, v1.0.2

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    node: true,
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+  },
+};
